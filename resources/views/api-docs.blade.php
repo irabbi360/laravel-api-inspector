9dac5548f07cebd4014934c9ed4b5f1cb86e321d
Fazle Rabbi <fazle08rabbi@gmail.com>
2025-12-23 21:30:42 +0600

    action class validation add & response schema update

--- resources/views/api-docs.blade.php
+++ resources/views/api-docs.blade.php
@@ -828,7 +828,25 @@
                     <div class="topbar-content">
                         <div class="api-info">
                             <div>
-                                <div class="api-title">@{{ apiInspector?.title || 'Laravel API Inspector' }}</div>
+                                <div class="api-title">@{{ apiInspector?.title || 'Laravel API Inspector' }} 
+                                    <a href="https://github.com/irabbi360/laravel-api-inspector" target="_blank">
+                                        <svg
+                                                width="30"
+                                                height="30"
+                                                viewBox="0 0 98 98"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                aria-hidden="true"
+                                        >
+                                            <path
+                                                    fill="#fff"
+                                                    fill-rule="evenodd"
+                                                    clip-rule="evenodd"
+                                                    d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z"
+                                            />
+                                        </svg>
+
+                                    </a>
+                                </div>
                                 <div class="api-version">
                                     Version <span class="version-badge">@{{ apiInspector?.version || '1.0.0' }}</span>
                                 </div>
@@ -850,7 +868,7 @@
                             </div>
                             <div v-if="loading" class="loading-spinner"></div>
                             <button v-else @click="$emit('refresh')" class="btn" style="background: #666;">↻ Refresh</button>
-                            <a href="https://github.com/irabbi360/laravel-api-inspector/issues/new" target="_blank" class="btn" style="background: #666;">Feature Request</a>
+                            <a href="https://github.com/irabbi360/laravel-api-inspector/issues/new" target="_blank" class="btn" style="background: #389f71ff;">Feature Request</a>
                         </div>
                     </div>
                 </div>
@@ -1139,19 +1157,44 @@
                 </div>
             `,
             methods: {
+                // formatSchema(schema) {
+                //     // Convert schema object to JSON format with proper indentation
+                //     const obj = {};
+                //     Object.keys(schema).forEach(key => {
+                //         const field = schema[key];
+                //         if (typeof field === 'object' && field !== null) {
+                //             obj[key] = field;
+                //         } else {
+                //             obj[key] = field;
+                //         }
+                //     });
+                //     return JSON.stringify(obj, null, 2);
+                // },
                 formatSchema(schema) {
-                    // Convert schema object to JSON format with proper indentation
                     const obj = {};
+
                     Object.keys(schema).forEach(key => {
                         const field = schema[key];
-                        if (typeof field === 'object' && field !== null) {
-                            obj[key] = field;
-                        } else {
+
+                        if (
+                            typeof field === 'object' &&
+                            field !== null &&
+                            field.schema &&
+                            typeof field.schema === 'object'
+                        ) {
+                            // Directly print schema under current parent
+                            obj[key] = this.formatSchema(field.schema);
+                        }
+                        else if (typeof field === 'object' && field !== null) {
+                            obj[key] = this.formatSchema(field);
+                        }
+                        else {
                             obj[key] = field;
                         }
                     });
-                    return JSON.stringify(obj, null, 2);
-                },
+
+                    return obj;
+                }
             },
         });
 
@@ -1293,6 +1336,7 @@
                         <div class="detail-body-left">
                             <ParametersSection :parameters="route?.parameters" />
                             <RequestBodySection :request-rules="route?.request_rules" />
+                            <ResponseSchema :schema="route?.response_schema" />
                         </div>
                         <div class="detail-body-right">
                             <RequestTester
@@ -1305,7 +1349,7 @@
                                 @update:pathParams="$emit('update:pathParams', $event)"
                                 @send-request="$emit('send-request')"
                             />
-                            <ResponseSchema :schema="route?.response_schema" />
+                            
                             <ResponseViewer
                                 :response="lastResponse"
                                 :schema="route?.response_schema"
--- src/Extractors/RequestRuleExtractor.php
+++ src/Extractors/RequestRuleExtractor.php
@@ -43,6 +43,13 @@ class RequestRuleExtractor
             return self::extractFromFormRequest($formRequestClass);
         }
 
+        // Check for Action class parameter
+        $actionClass = self::hasActionParameter($reflectionMethod);
+
+        if ($actionClass) {
+            return self::extractFromAction($actionClass);
+        }
+
         return [];
     }
 
@@ -70,6 +77,434 @@ class RequestRuleExtractor
         }
 
         return [];
+    }
+
+    /**
+     * Check if method has an Action class parameter
+     */
+    public static function hasActionParameter(\ReflectionMethod $method): ?string
+    {
+        foreach ($method->getParameters() as $param) {
+            $paramType = $param->getType();
+
+            if (! $paramType instanceof \ReflectionNamedType || $paramType->isBuiltin()) {
+                continue;
+            }
+
+            $paramClass = $paramType->getName();
+
+            // Check if class exists and has any rules method (Action pattern)
+            if (class_exists($paramClass) && self::hasRulesMethod($paramClass)) {
+                return $paramClass;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Check if Action class has any rules method
+     */
+    public static function hasRulesMethod(string $class): bool
+    {
+        $methodNames = ['rules', 'getRules', 'getValidationRules', 'validate'];
+
+        foreach ($methodNames as $methodName) {
+            if (method_exists($class, $methodName)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Extract rules from an Action class
+     *
+     * @return array<string, array>
+     */
+    public static function extractFromAction(string $actionClass): array
+    {
+        if (! class_exists($actionClass)) {
+            return [];
+        }
+
+        // Try different rule method names in priority order
+        $methodNames = ['rules', 'getRules', 'getValidationRules'];
+
+        foreach ($methodNames as $methodName) {
+            if (method_exists($actionClass, $methodName)) {
+                $rules = self::extractRulesFromMethod($actionClass, $methodName);
+                if (! empty($rules)) {
+                    return $rules;
+                }
+            }
+        }
+
+        // Try to extract from validate() method
+        if (method_exists($actionClass, 'validate')) {
+            $rules = self::extractRulesFromValidateMethod($actionClass);
+            if (! empty($rules)) {
+                return $rules;
+            }
+        }
+
+        return [];
+    }
+
+    /**
+     * Extract rules by calling a specific method
+     *
+     * @return array<string, array>
+     */
+    private static function extractRulesFromMethod(string $actionClass, string $methodName): array
+    {
+        try {
+            $instance = new $actionClass;
+
+            if (method_exists($instance, $methodName)) {
+                $rules = $instance->{$methodName}();
+
+                // Only parse if rules is an array
+                if (is_array($rules)) {
+                    return RuleParser::parse($rules);
+                }
+            }
+        } catch (\Exception) {
+            // Silently fail
+        }
+
+        return [];
+    }
+
+    /**
+     * Extract rules from a validate() method by analyzing its code
+     *
+     * @return array<string, array>
+     */
+    private static function extractRulesFromValidateMethod(string $actionClass): array
+    {
+        try {
+            $reflection = new \ReflectionClass($actionClass);
+            if (! $reflection->hasMethod('validate')) {
+                return [];
+            }
+
+            $method = $reflection->getMethod('validate');
+            $file = $method->getFileName();
+            $startLine = $method->getStartLine();
+            $endLine = $method->getEndLine();
+
+            if (! file_exists($file)) {
+                return [];
+            }
+
+            $lines = file($file);
+            $methodCode = implode('', array_slice($lines, $startLine - 1, $endLine - $startLine + 1));
+
+            // First try to extract rules from Validator::make() or validator() call
+            $rulesFromValidator = self::parseValidatorRulesFromCode($methodCode);
+            if (! empty($rulesFromValidator)) {
+                return $rulesFromValidator;
+            }
+
+            // If no Validator::make() found, try to extract rules array from return statement
+            if (preg_match('/return\s*(\[.+?\];?)/s', $methodCode, $matches)) {
+                $rulesCode = $matches[1];
+                
+                // Remove trailing semicolon if present
+                $rulesCode = rtrim($rulesCode, '; \t\n\r');
+                
+                // Try to evaluate as PHP array (safely with regex parsing)
+                if (strpos($rulesCode, '[') === 0 && strpos($rulesCode, ']') === strlen($rulesCode) - 1) {
+                    return self::extractRulesUsingRegex($rulesCode);
+                }
+            }
+        } catch (\Exception) {
+            // Silently fail
+        }
+
+        return [];
+    }
+
+    /**
+     * Parse validation rules from Validator::make() code
+     *
+     * @return array<string, array>
+     */
+    private static function parseValidatorRulesFromCode(string $code): array
+    {
+        // Look for Validator::make($input, [ ... ]) or validator($input, [ ... ])
+        // Find the opening bracket after the comma
+        if (preg_match('/(?:Validator::make|validator)\s*\([^,]+,\s*(\[)/s', $code, $matches)) {
+            // Find the position where the array starts
+            $arrayStart = strpos($code, '[', strpos($code, ',')) + 1;
+            
+            // Extract the complete rules array by counting brackets
+            $bracketCount = 0;
+            $rulesCode = '[';
+            $foundStart = false;
+            
+            for ($i = $arrayStart; $i < strlen($code); $i++) {
+                $char = $code[$i];
+                
+                if ($char === '[') {
+                    $bracketCount++;
+                    $foundStart = true;
+                    $rulesCode .= $char;
+                } elseif ($char === ']') {
+                    if ($bracketCount === 0) {
+                        // Found the closing bracket of the rules array
+                        $rulesCode .= $char;
+                        break;
+                    } else {
+                        $bracketCount--;
+                        $rulesCode .= $char;
+                    }
+                } else {
+                    $rulesCode .= $char;
+                }
+            }
+            
+            if (strlen($rulesCode) > 2) {
+                // Use regex extraction instead of eval to avoid syntax errors
+                return self::extractRulesUsingRegex($rulesCode);
+            }
+        }
+
+        return [];
+    }
+
+    /**
+     * Extract rules using regex patterns and bracket-aware splitting
+     *
+     * @return array<string, array>
+     */
+    private static function extractRulesUsingRegex(string $rulesContent): array
+    {
+        $rules = [];
+
+        // First, extract the content between outer brackets
+        $trimmed = trim($rulesContent);
+        if (strpos($trimmed, '[') === 0 && strrpos($trimmed, ']') === strlen($trimmed) - 1) {
+            $innerContent = trim(substr($trimmed, 1, -1));
+        } else {
+            $innerContent = trim($rulesContent, '[] \t\n\r');
+        }
+
+        // Split by top-level commas to get individual field => rules pairs
+        $fields = self::splitFieldDefinitions($innerContent);
+
+        foreach ($fields as $field) {
+            $field = trim($field);
+            
+            // Skip empty fields
+            if (empty($field)) {
+                continue;
+            }
+            
+            // Match: 'fieldName' => rules or "fieldName" => rules
+            if (preg_match('/^[\'"]([^\'"]+)[\'"]\s*=>\s*(.+)$/s', $field, $match)) {
+                $fieldName = $match[1];
+                $ruleValue = trim($match[2]);
+
+                // Remove trailing commas and whitespace
+                $ruleValue = rtrim($ruleValue, ',; \t\n\r');
+
+                if (! empty($ruleValue)) {
+                    // Extract simple string rules: 'rule|rule|rule'
+                    if (strpos($ruleValue, '[') === 0) {
+                        // Array format: ['rule', 'rule', 'rule']
+                        // Extract strings and rule objects from array
+                        $rules[$fieldName] = self::extractRulesFromArray($ruleValue);
+                    } else {
+                        // String format: 'rule|rule|rule'
+                        $rules[$fieldName] = $ruleValue;
+                    }
+                }
+            }
+        }
+
+        // Parse the collected rules
+        if (! empty($rules)) {
+            return RuleParser::parse($rules);
+        }
+
+        return [];
+    }
+
+    /**
+     * Split field definitions by top-level comma (respecting brackets and parentheses)
+     *
+     * @return array<string>
+     */
+    private static function splitFieldDefinitions(string $content): array
+    {
+        $fields = [];
+        $current = '';
+        $bracketDepth = 0;
+        $parenDepth = 0;
+        $inString = false;
+        $stringChar = '';
+
+        for ($i = 0; $i < strlen($content); $i++) {
+            $char = $content[$i];
+            
+            // Handle string delimiters
+            if (($char === '"' || $char === "'") && ($i === 0 || $content[$i - 1] !== '\\')) {
+                if (! $inString) {
+                    $inString = true;
+                    $stringChar = $char;
+                } elseif ($char === $stringChar) {
+                    $inString = false;
+                }
+            }
+
+            // If we're inside a string, just append the character
+            if ($inString) {
+                $current .= $char;
+                continue;
+            }
+
+            switch ($char) {
+                case '[':
+                    $bracketDepth++;
+                    $current .= $char;
+                    break;
+                case ']':
+                    $bracketDepth--;
+                    $current .= $char;
+                    break;
+                case '(':
+                    $parenDepth++;
+                    $current .= $char;
+                    break;
+                case ')':
+                    $parenDepth--;
+                    $current .= $char;
+                    break;
+                case ',':
+                    if ($bracketDepth === 0 && $parenDepth === 0) {
+                        if (! empty($current)) {
+                            $fields[] = $current;
+                            $current = '';
+                        }
+                    } else {
+                        $current .= $char;
+                    }
+                    break;
+                default:
+                    $current .= $char;
+            }
+        }
+
+        if (! empty($current)) {
+            $fields[] = $current;
+        }
+
+        return $fields;
+    }
+
+    /**
+     * Split rules by top-level comma (respecting nested brackets)
+     *
+     * @return array<string>
+     */
+    private static function splitRulesByTopLevelComma(string $content): array
+    {
+        $parts = [];
+        $current = '';
+        $bracketDepth = 0;
+        $parenDepth = 0;
+
+        for ($i = 0; $i < strlen($content); $i++) {
+            $char = $content[$i];
+
+            switch ($char) {
+                case '[':
+                    $bracketDepth++;
+                    $current .= $char;
+                    break;
+                case ']':
+                    $bracketDepth--;
+                    $current .= $char;
+                    break;
+                case '(':
+                    $parenDepth++;
+                    $current .= $char;
+                    break;
+                case ')':
+                    $parenDepth--;
+                    $current .= $char;
+                    break;
+                case ',':
+                    if ($bracketDepth === 0 && $parenDepth === 0) {
+                        if (! empty($current)) {
+                            $parts[] = $current;
+                            $current = '';
+                        }
+                    } else {
+                        $current .= $char;
+                    }
+                    break;
+                default:
+                    $current .= $char;
+            }
+        }
+
+        if (! empty($current)) {
+            $parts[] = $current;
+        }
+
+        return $parts;
+    }
+
+    /**
+     * Extract rules from array format like ['required', 'email', Rule::exists(...)]
+     *
+     * @return string
+     */
+    private static function extractRulesFromArray(string $arrayStr): string
+    {
+        // Remove outer brackets
+        $arrayStr = trim($arrayStr, '[]');
+
+        // Split by comma at the top level
+        $items = self::splitRulesByTopLevelComma('[' . $arrayStr . ']');
+
+        $rules = [];
+        foreach ($items as $item) {
+            $item = trim($item);
+
+            // Remove leading/trailing brackets if they wrap the whole thing
+            if (str_starts_with($item, '[') && str_ends_with($item, ']')) {
+                $item = trim($item, '[]');
+            }
+
+            // Extract string rules: 'required', "required", required
+            if (preg_match('/[\'"]?([a-z_:0-9|]+)[\'"]?/i', $item, $match)) {
+                $rule = trim($match[1], '\'\"');
+                if (! empty($rule) && ! str_contains($rule, '(') && ! str_contains($rule, 'Rule::')) {
+                    $rules[] = $rule;
+                }
+            }
+            // For Rule objects, try to extract the rule from the PHP code
+            elseif (preg_match('/Rule::(\w+)\s*\(\s*([^)]+)\s*\)/i', $item, $match)) {
+                $ruleName = $match[1];
+                $ruleArgs = trim($match[2]);
+
+                // Handle common Rule:: methods
+                if (strtolower($ruleName) === 'exists') {
+                    $rules[] = 'exists:' . str_replace(['\'', '"'], '', $ruleArgs);
+                } elseif (strtolower($ruleName) === 'unique') {
+                    $rules[] = 'unique:' . str_replace(['\'', '"'], '', $ruleArgs);
+                } elseif (strtolower($ruleName) === 'in') {
+                    $rules[] = 'in:' . str_replace(['\'', '"'], '', $ruleArgs);
+                }
+            }
+        }
+
+        return implode('|', $rules);
     }
 
     /**
