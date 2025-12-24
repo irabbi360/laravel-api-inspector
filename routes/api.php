21d087beac85f280347364fc1cc56cff2e22327b
Fazle Rabbi <fazle08rabbi@gmail.com>
2025-12-24 22:36:07 +0600

    enabled config implement

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use Irabbi360\LaravelApiInspector\Http\Controllers\ApiInspectorController;
+use Irabbi360\LaravelApiInspector\Http\Middleware\ApiInspectorMiddleware;
 
-Route::middleware('api')->prefix('api')->group(function () {
+Route::middleware(['api', ApiInspectorMiddleware::class])->prefix('api')->group(function () {
     Route::get('api-inspector-docs', [ApiInspectorController::class, 'fetchApiInfo'])->name('api-inspector.docs.fetch');
     Route::get('api-inspector-docs/postman', [ApiInspectorController::class, 'postman'])->name('api-inspector.docs.postman');
     Route::get('api-inspector-docs/openapi', [ApiInspectorController::class, 'openapi'])->name('api-inspector.docs.openapi');
--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use Irabbi360\LaravelApiInspector\Http\Controllers\ApiInspectorController;
+use Irabbi360\LaravelApiInspector\Http\Middleware\ApiInspectorMiddleware;
 
 // Get the configured route path
 $routePath = config('api-inspector.route_path', 'api-docs');
 
-Route::middleware('web')->group(function () use ($routePath) {
+Route::middleware(['web', ApiInspectorMiddleware::class])->group(function () use ($routePath) {
     // API Inspector SPA routes - dynamic based on config
     Route::get("/{$routePath}/{view?}", [ApiInspectorController::class, 'index'])
         ->where('view', '(.*)')
